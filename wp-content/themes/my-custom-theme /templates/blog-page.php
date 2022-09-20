b81db96bab729c9d039879319a6b6e1bf9107e78
Rasallsss <rasalmdn123@gmail.com>
2022-09-20 18:31:42 +0530

    end 20

--- wp-content/themes/my-custom-theme /templates/blog-page.php
+++ wp-content/themes/my-custom-theme /templates/blog-page.php
@@ -3,6 +3,34 @@
 <?php
 get_header();
 ?>
+        <section class="life-style-section">
+        <div class="main-parent">
+            <?php
+                $slug = 'lifestyle';
+                $cat = get_category_by_slug($slug);
+                $catID = $cat->term_id;
+//-------------------------------------- posts ----------------------------------------//
+                $argmnd = array ('category' => $catID);
+                $post = get_posts($argmnd);
+                if ( have_posts() ){
+                    // echo 'its currect';
+                    foreach($post as $eachPost){
+                        // var_dump($eachPost);
+                        ?>
+                            <div class="flex-section">
+                                <div class="headding-section">
+                                    <h1><?php echo $eachPost->post_title?></h1>
+                            </div>
+                            <div class="image-section-1">
+                                <?php echo $eachPost->post_content; ?>
+                            </div>
+                        </div>
+                        <?php
+                    }
+                }
+                ?>
+        </div>
+    </section>
 
 
 
